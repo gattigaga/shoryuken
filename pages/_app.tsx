a8e8ff868fdf3f8a8c0c731eea70ba52f680d427
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-17 01:51:18 +0700

    Select certain font-weights of @fontsource/poppins

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
-import '../styles/globals.css'
-import "@fontsource/poppins"
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/400-italic.css";
+import "@fontsource/poppins/600-italic.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
