c4a786738eda921b15d6a635837e6d1ca445d0bc
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-17 02:38:29 +0700

    Create Button component

---
+++ components/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type Props = JSX.IntrinsicElements["button"];
+
+const Button: React.FC<Props> = ({ className, children, ...rest }) => {
+  return (
+    <button
+      className={`bg-blue-500 text-white px-2 py-3 rounded font-sans text-xs font-semibold outline-blue-400 ${className}`}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
