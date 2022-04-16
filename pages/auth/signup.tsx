609e38193e48ccba62c15551bedac80a6b633526
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-17 02:00:01 +0700

    Create blank signup page

---
+++ pages/auth/signup.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Sign Up | Shoryuken</title>
+      </Head>
+
+      <main></main>
+    </div>
+  );
+};
+
+export default SignUpPage;
