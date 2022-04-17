8fff92067c2af40d8c27d7547b0232866f506753
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-17 19:03:57 +0700

    Create blank signin page

---
+++ pages/auth/signin.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const SignInPage: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Sign In | Shoryuken</title>
+      </Head>
+
+      <main></main>
+    </div>
+  );
+};
+
+export default SignInPage;
