210af1874206f3d6e915ce1f54be7c0e1a17ac3a
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-17 15:00:17 +0700

    Create blank dashboard page

---
+++ pages/dashboard.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const DashboardPage: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Dashboard | Shoryuken</title>
+      </Head>
+
+      <main></main>
+    </div>
+  );
+};
+
+export default DashboardPage;
