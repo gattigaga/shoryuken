b36253137958041267a1a56de133904aba50fe4f
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-17 16:53:56 +0700

    Install axios, react-spinners, and react-hot-toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,15 @@ import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/400-italic.css";
 import "@fontsource/poppins/600-italic.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
 }
 
 export default MyApp;
