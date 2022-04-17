e7200a24c703fd1a8329e84f0fbe83a3d9dbdb2e
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-17 15:41:43 +0700

    Fix colors in Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,7 +5,7 @@ type Props = JSX.IntrinsicElements["button"];
 const Button: React.FC<Props> = ({ className, children, ...rest }) => {
   return (
     <button
-      className={`bg-blue-700 text-white px-2 py-3 rounded font-sans text-xs font-semibold outline-blue-400 ${className}`}
+      className={`bg-blue-700 text-white px-2 py-3 rounded font-sans text-xs font-semibold outline-blue-400 hover:bg-blue-800 disabled:bg-slate-200 disabled:text-slate-500 ${className}`}
       {...rest}
     >
       {children}
