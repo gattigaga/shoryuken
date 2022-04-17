c9c6a2d0a5b538683de9c237a387e4b88127dcbb
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-17 19:57:43 +0700

    Create "POST /auth/signin" api

---
+++ pages/api/auth/signin.ts
@@ -0,0 +1,47 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ApiError } from "@supabase/supabase-js";
+
+import supabase from "../../../helpers/supabase";
+
+type Content = {
+  data?: {
+    user: any;
+    session: any;
+  };
+  message: string;
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<Content>) => {
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Method not allowed." });
+    return;
+  }
+
+  try {
+    const { email, password } = req.body as {
+      email: string;
+      password: string;
+    };
+
+    const { user, session, error } = await supabase.auth.signIn({
+      email,
+      password,
+    });
+
+    if (error) {
+      throw error;
+    }
+
+    res.status(200).json({
+      data: {
+        user,
+        session,
+      },
+      message: "User successfully signed in.",
+    });
+  } catch (error: any) {
+    res.status(error.status).json({ message: error.message });
+  }
+};
+
+export default handler;
