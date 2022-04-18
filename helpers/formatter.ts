a4e3bbb64291c4dab086cf1dae5dc7a9c6f0b521
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-19 00:31:48 +0700

    Create "getSlug" helper

--- helpers/formatter.ts
+++ helpers/formatter.ts
@@ -1,3 +1,5 @@
+import { v4 as uuid } from "uuid";
+
 export const getExcerpt = (words: string, maxChars = 20) => {
   const isExcerptNeeded = words.length > maxChars;
 
@@ -6,4 +8,12 @@ export const getExcerpt = (words: string, maxChars = 20) => {
   }
 
   return words;
+};
+
+export const getSlug = (words: string) => {
+  const uniqueCode = uuid().split("-")[0];
+
+  return (
+    words.toLowerCase().split(" ").slice(0, 5).join("-") + `-${uniqueCode}`
+  );
 };
