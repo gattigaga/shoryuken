a34ee18ca6f4e5a1ec43d11271eb2a24b6291613
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-19 00:57:31 +0700

    Install and setup react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,15 @@ import "@fontsource/poppins/600-italic.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import Modal from "react-modal";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 Modal.setAppElement("#__next");
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
       <Toaster
         toastOptions={{
@@ -23,7 +26,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         }}
         position="top-right"
       />
-    </>
+    </QueryClientProvider>
   );
 }
 
