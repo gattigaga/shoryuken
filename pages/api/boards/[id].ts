92cf36e237abf34f4fba29c46ef81956b8d90f46
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-19 00:13:22 +0700

    Create "GET /boards/[id]" and "PUT /boards/[id]" api

---
+++ pages/api/boards/[id].ts
@@ -0,0 +1,83 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import supabase from "../../../helpers/supabase";
+
+type Content = {
+  data?: any;
+  message: string;
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<Content>) => {
+  if (!["GET", "PUT"].includes(req.method || "")) {
+    res.status(405).json({ message: "Method not allowed." });
+    return;
+  }
+
+  const token = req.headers.authorization?.split(" ")[1] || "";
+
+  if (req.method === "GET") {
+    try {
+      const { error: userError } = await supabase.auth.api.getUser(token);
+
+      if (userError) {
+        throw userError;
+      }
+
+      const { id } = req.query as {
+        id: string;
+      };
+
+      const { data: boards, error: boardsError } = await supabase
+        .from("boards")
+        .select("*")
+        .eq("id", id);
+
+      if (boardsError) {
+        throw boardsError;
+      }
+
+      res.status(200).json({
+        data: boards[0],
+        message: "There's existing board.",
+      });
+    } catch (error: any) {
+      res.status(error.status).json({ message: error.message });
+    }
+  }
+
+  if (req.method === "PUT") {
+    try {
+      const { error: userError } = await supabase.auth.api.getUser(token);
+
+      if (userError) {
+        throw userError;
+      }
+
+      const { id } = req.query as {
+        id: string;
+      };
+
+      const { title } = req.body as {
+        title: string;
+      };
+
+      const { data: boards, error: boardsError } = await supabase
+        .from("boards")
+        .update({ title })
+        .eq("id", id);
+
+      if (boardsError) {
+        throw boardsError;
+      }
+
+      res.status(200).json({
+        data: boards[0],
+        message: "Board successfully updated.",
+      });
+    } catch (error: any) {
+      res.status(error.status).json({ message: error.message });
+    }
+  }
+};
+
+export default handler;
