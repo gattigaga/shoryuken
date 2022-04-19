1f340c7531780544414b65765cf638e729d51f55
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-19 21:56:26 +0700

    Make dashboard page able to load boards

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,15 +1,55 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+import Board from "../components/Board";
+import CreateBoardButton from "../components/CreateBoardButton";
+import ModalCreateBoard from "../components/ModalCreateBoard";
+import { getBoards } from "../api/boards";
+import Layout from "../components/Layout";
+import { getMe } from "../api/user";
 
 const DashboardPage: NextPage = () => {
+  const [isCreateBoardOpen, setIsCreateBoardOpen] = useState(false);
+  const { data: myself } = useQuery("me", getMe);
+
+  const { data: boards } = useQuery("boards", getBoards, {
+    initialData: [],
+  });
+
+  const username = myself?.user_metadata?.username || "";
+
   return (
-    <div>
+    <Layout>
       <Head>
         <title>Dashboard | Shoryuken</title>
       </Head>
 
-      <main></main>
-    </div>
+      <div className="px-8 pt-12 pb-32">
+        <div className="w-full mx-auto sm:w-5/6">
+          <h1 className="text-2xl font-semibold text-slate-600 mb-8">
+            My Boards
+          </h1>
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {boards.map((board: any) => {
+              return (
+                <Board
+                  key={board.id}
+                  title={board.title}
+                  href={`/${username}/${board.slug}`}
+                />
+              );
+            })}
+            <CreateBoardButton onClick={() => setIsCreateBoardOpen(true)} />
+          </div>
+        </div>
+      </div>
+      <ModalCreateBoard
+        onRequestClose={() => setIsCreateBoardOpen(false)}
+        isOpen={isCreateBoardOpen}
+      />
+    </Layout>
   );
 };
 
