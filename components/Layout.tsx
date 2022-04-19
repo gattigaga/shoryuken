45055c49cddda6a4133e9ac6749e9cee2386997c
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-19 19:08:47 +0700

    Create Layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+type Props = {
+  children: any;
+};
+
+const Layout: React.FC<Props> = ({ children }) => {
+  return (
+    <div className="h-screen bg-slate-50 flex flex-col">
+      <NavBar />
+
+      <main className="flex-1 overflow-auto">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
