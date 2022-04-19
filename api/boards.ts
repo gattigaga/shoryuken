3805e0d794d2a90e24b06ee45a6aa4f0561eb6eb
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-19 23:47:23 +0700

    Add more api call definition for boards

--- api/boards.ts
+++ api/boards.ts
@@ -11,3 +11,16 @@ export const postBoard = async (body: { title: string }) => {
 
   return res.data.data;
 };
+
+export const getBoardById = (id: string | number) => async () => {
+  const res = await axios.get(`/api/boards/${id}`);
+
+  return res.data.data;
+};
+
+export const putBoardById =
+  (id: string | number) => async (body: { title: string }) => {
+    const res = await axios.put(`/api/boards/${id}`, body);
+
+    return res.data.data;
+  };
