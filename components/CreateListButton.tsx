7c39799f77e5a6c2253aa16ac3b5f6e06f1266ef
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-23 17:52:36 +0700

    Create CreateListButton component

---
+++ components/CreateListButton.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { MdAdd } from "react-icons/md";
+
+type Props = {
+  onClick?: () => void;
+};
+
+const CreateListButton: React.FC<Props> = ({ onClick }) => {
+  return (
+    <button
+      className="px-4 w-80 h-12 bg-blue-500 rounded flex items-center hover:bg-blue-400"
+      type="button"
+      onClick={onClick}
+    >
+      <MdAdd color="white" size={24} />
+      <p className="ml-2 text-xs text-white">Create new list</p>
+    </button>
+  );
+};
+
+export default CreateListButton;
