1a50f1cf158886e4014c614285de25ad09e8c9e8
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-30 23:44:03 +0700

    Make List component to support children

--- components/List.tsx
+++ components/List.tsx
@@ -8,6 +8,7 @@ type Props = {
   id: string | number;
   index: number;
   title: string;
+  children?: JSX.Element | JSX.Element[];
   onSubmitTitle?: (value: string) => void;
   onClickRemove?: () => void;
   onClickAddCard?: (value: string) => void;
@@ -17,6 +18,7 @@ const List: React.FC<Props> = ({
   id,
   index,
   title,
+  children,
   onSubmitTitle,
   onClickRemove,
   onClickAddCard,
@@ -26,6 +28,13 @@ const List: React.FC<Props> = ({
   const [cardTitle, setCardTitle] = useState("");
   const refListTitleInput = useRef<HTMLInputElement>(null);
   const refCardTitleInput = useRef<HTMLTextAreaElement>(null);
+
+  const maxHeight = (() => {
+    const viewportHeight = window?.innerHeight || 0;
+    const footerHeight = isCreateCardFormOpen ? 48 : 0;
+
+    return viewportHeight * 0.6 + footerHeight;
+  })();
 
   // Set focus on list title input if title editing form opened.
   useEffect(() => {
@@ -46,11 +55,12 @@ const List: React.FC<Props> = ({
       {(provided) => (
         <div
           ref={provided.innerRef}
-          className="shrink-0 w-72 p-2 bg-slate-300 rounded mr-2"
+          className="shrink-0 w-72 bg-slate-300 rounded mr-2 flex flex-col"
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          <div className="w-full flex items-center mb-2">
+          {/* Header */}
+          <div className="w-full flex items-center px-2 h-12">
             {!isEditingTitle ? (
               <button
                 className="flex-1 text-left px-2 py-1 text-sm text-slate-700 font-semibold"
@@ -84,68 +94,78 @@ const List: React.FC<Props> = ({
               <MdClose size={24} />
             </button>
           </div>
-          {isCreateCardFormOpen ? (
-            <>
-              <div className="bg-white rounded shadow mb-2">
-                <textarea
-                  ref={refCardTitleInput}
-                  className="w-full p-2 border-0 resize-none rounded text-xs h-16 focus:ring-0"
-                  value={cardTitle}
-                  placeholder="Enter a title for this card..."
-                  onChange={(event) => setCardTitle(event.target.value)}
-                  onKeyDown={(event) => {
-                    switch (event.key) {
-                      case "Enter":
-                        event.preventDefault();
-                        onClickAddCard?.(cardTitle);
-                        setIsCreateCardFormOpen(false);
-                        setCardTitle("");
-                        break;
 
-                      case "Escape":
-                        setIsCreateCardFormOpen(false);
-                        setCardTitle("");
-                        break;
+          {/* Children */}
+          <div style={{ maxHeight }} className="overflow-auto px-2">
+            {children}
+            {isCreateCardFormOpen && (
+              <div className="py-2">
+                <div className="bg-white rounded shadow mb-2">
+                  <textarea
+                    ref={refCardTitleInput}
+                    className="w-full p-2 border-0 resize-none rounded text-xs h-16 focus:ring-0"
+                    value={cardTitle}
+                    placeholder="Enter a title for this card..."
+                    onChange={(event) => setCardTitle(event.target.value)}
+                    onKeyDown={(event) => {
+                      switch (event.key) {
+                        case "Enter":
+                          event.preventDefault();
+                          onClickAddCard?.(cardTitle);
+                          setIsCreateCardFormOpen(false);
+                          setCardTitle("");
+                          break;
 
-                      default:
-                        break;
-                    }
-                  }}
-                />
+                        case "Escape":
+                          setIsCreateCardFormOpen(false);
+                          setCardTitle("");
+                          break;
+
+                        default:
+                          break;
+                      }
+                    }}
+                  />
+                </div>
+                <div className="flex items-center">
+                  <Button
+                    className="outline-black mr-2"
+                    type="button"
+                    onClick={() => {
+                      onClickAddCard?.(cardTitle);
+                      setIsCreateCardFormOpen(false);
+                      setCardTitle("");
+                    }}
+                  >
+                    Add card
+                  </Button>
+                  <button
+                    className="text-slate-500 hover:text-slate-600"
+                    type="button"
+                    onClick={() => {
+                      setIsCreateCardFormOpen(false);
+                      setCardTitle("");
+                    }}
+                  >
+                    <MdClose size={24} />
+                  </button>
+                </div>
               </div>
-              <div className="flex items-center">
-                <Button
-                  className="outline-black mr-2"
-                  type="button"
-                  onClick={() => {
-                    onClickAddCard?.(cardTitle);
-                    setIsCreateCardFormOpen(false);
-                    setCardTitle("");
-                  }}
-                >
-                  Add card
-                </Button>
-                <button
-                  className="text-slate-500 hover:text-slate-600"
-                  type="button"
-                  onClick={() => {
-                    setIsCreateCardFormOpen(false);
-                    setCardTitle("");
-                  }}
-                >
-                  <MdClose size={24} />
-                </button>
-              </div>
-            </>
-          ) : (
-            <button
-              className="w-full px-2 py-1 rounded flex items-center text-slate-500 hover:text-slate-600 hover:bg-slate-400"
-              type="button"
-              onClick={() => setIsCreateCardFormOpen(true)}
-            >
-              <MdAdd size={24} />
-              <p className="ml-2 text-xs">Add a card</p>
-            </button>
+            )}
+          </div>
+
+          {/* Footer */}
+          {!isCreateCardFormOpen && (
+            <div className="px-2 h-12 flex items-center">
+              <button
+                className="w-full px-2 py-1 rounded flex items-center text-slate-500 hover:text-slate-600 hover:bg-slate-400"
+                type="button"
+                onClick={() => setIsCreateCardFormOpen(true)}
+              >
+                <MdAdd size={24} />
+                <p className="ml-2 text-xs">Add a card</p>
+              </button>
+            </div>
           )}
         </div>
       )}
