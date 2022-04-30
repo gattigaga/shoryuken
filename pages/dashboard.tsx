8a299d9d71ab07a20a23ac5de039cc8b838d07a6
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-04-30 12:30:20 +0700

    Prevent unauthenticated user to redirect to dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
 import { useQuery } from "react-query";
@@ -9,6 +9,24 @@ import ModalCreateBoard from "../components/ModalCreateBoard";
 import { getBoards } from "../api/boards";
 import Layout from "../components/Layout";
 import { getMe } from "../api/user";
+import { getAuthStatus } from "../helpers/server";
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const isAuthenticated = await getAuthStatus(req.headers?.cookie || "");
+
+  if (!isAuthenticated) {
+    return {
+      redirect: {
+        destination: "/auth/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
 
 const DashboardPage: NextPage = () => {
   const [isCreateBoardOpen, setIsCreateBoardOpen] = useState(false);
