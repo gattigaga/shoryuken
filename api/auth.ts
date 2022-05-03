54e42917cbac39391405ceef564d1b4b41f43e2c
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-05-03 18:22:33 +0700

    Remove api call definition for auth and user

--- api/auth.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const postSignOut = async () => {
-  const res = await axios.post("/api/auth/signout");
-
-  return res.data.data;
-};
--- api/user.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const getMe = async () => {
-  const res = await axios.get("/api/auth/me");
-
-  return res.data.data;
-};
