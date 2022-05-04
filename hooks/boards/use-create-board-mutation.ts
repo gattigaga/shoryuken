4fe639e22f44bd3046241284dd3b9746c8a3126e
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-05-04 10:09:43 +0700

    Implement optimistic update in useCreateBoardMutation hook

--- hooks/boards/use-create-board-mutation.ts
+++ hooks/boards/use-create-board-mutation.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
+import { v4 as uuid } from "uuid";
+
+import { getSlug } from "../../helpers/formatter";
 
 type Response = any;
 
@@ -14,6 +17,37 @@ const createBoard = async (body: Body): Promise<Response> => {
   return data;
 };
 
-const useCreateBoardMutation = () => useMutation(createBoard);
+const useCreateBoardMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation(createBoard, {
+    onMutate: async (payload) => {
+      await queryClient.cancelQueries("boards");
+
+      const previousBoards = queryClient.getQueryData("boards");
+      const myself = queryClient.getQueryData("me");
+
+      const newBoard = {
+        id: uuid(),
+        user_id: myself?.id,
+        slug: getSlug(payload.title),
+        ...payload,
+      };
+
+      queryClient.setQueryData("boards", (oldBoards) => [
+        ...oldBoards,
+        newBoard,
+      ]);
+
+      return { previousBoards };
+    },
+    onError: (error, payload, context) => {
+      queryClient.setQueryData("boards", context.previousBoards);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries("boards");
+    },
+  });
+};
 
 export default useCreateBoardMutation;
