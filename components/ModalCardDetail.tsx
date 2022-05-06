1a72074404f93995c9b681ff764d20aba550f0a3
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-05-06 19:07:05 +0700

    Create delete card feature

--- components/ModalCardDetail.tsx
+++ components/ModalCardDetail.tsx
@@ -1,20 +1,45 @@
 import Head from "next/head";
 import Modal from "react-modal";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { MdClose } from "react-icons/md";
+import { MdClose, MdDelete } from "react-icons/md";
+import toast from "react-hot-toast";
 
 import useCardQuery from "../hooks/cards/use-card-query";
+import useDeleteCardMutation from "../hooks/cards/use-delete-card-mutation";
 import CardTitle from "./CardTitle";
 import CardDescription from "./CardDescription";
 
 type Props = {
   id: number;
   isOpen: boolean;
-  onRequestClose?: () => void;
 };
 
-const ModalCardDetail: React.FC<Props> = ({ id, isOpen, onRequestClose }) => {
+const ModalCardDetail: React.FC<Props> = ({ id, isOpen }) => {
   const { data: card, refetch } = useCardQuery(id);
+  const deleteCardMutation = useDeleteCardMutation();
+  const router = useRouter();
+
+  const path = `/boards/${router.query.slug}`;
+
+  const close = () => router.push(path);
+
+  const deleteCard = async () => {
+    const isYes = confirm("Are you sure you want to delete this card ?");
+
+    if (isYes) {
+      try {
+        await close();
+
+        await deleteCardMutation.mutateAsync({
+          id,
+          listId: card.list_id,
+        });
+      } catch (error) {
+        toast.error("Failed to delete a card.");
+      }
+    }
+  };
 
   useEffect(() => {
     if (isOpen) {
@@ -25,7 +50,7 @@ const ModalCardDetail: React.FC<Props> = ({ id, isOpen, onRequestClose }) => {
   return (
     <Modal
       isOpen={isOpen}
-      onRequestClose={onRequestClose}
+      onRequestClose={close}
       style={{
         overlay: {
           background: "rgba(0, 0, 0, 0.5)",
@@ -49,18 +74,39 @@ const ModalCardDetail: React.FC<Props> = ({ id, isOpen, onRequestClose }) => {
       </Head>
 
       <div className="py-12 w-full">
-        <div className="w-full rounded bg-slate-200 p-5 pb-10">
+        <div className="w-full rounded bg-slate-100 p-5 pb-10">
+          {/* Header Side */}
           <div className="flex items-start">
             <CardTitle id={id} />
             <button
               className="text-slate-500 ml-12"
               type="button"
-              onClick={onRequestClose}
+              onClick={close}
             >
               <MdClose size={24} />
             </button>
           </div>
-          <CardDescription id={id} />
+
+          <div className="flex">
+            <div className="flex-1">
+              <CardDescription id={id} />
+            </div>
+
+            {/* Right Side Menu */}
+            <div className="w-48 ml-16">
+              <p className="font-semibold text-xs text-slate-700 mb-3">
+                Actions
+              </p>
+              <button
+                className="flex items-center rounded bg-red-700 hover:bg-red-800 text-white w-full px-2 py-2"
+                type="button"
+                onClick={deleteCard}
+              >
+                <MdDelete size={20} />
+                <p className="text-sm ml-3">Delete</p>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </Modal>
