4af423c47621eaec34e1434501a7cdbdf162de2f
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-05-09 23:53:25 +0700

    Disable type check when building for production

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // TODO: Temporarily, should be removed soon.
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
