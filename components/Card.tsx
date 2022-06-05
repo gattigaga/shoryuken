99d9dd60363950559cc38f0b23b82f12bb9506e8
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-06-05 20:57:11 +0700

    Add checklist support in Card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import React from "react";
-import { MdEdit, MdSubject } from "react-icons/md";
+import { MdEdit, MdOutlineCheckBox, MdSubject } from "react-icons/md";
 import { Draggable } from "react-beautiful-dnd";
+import classnames from "classnames";
 
 import useBoardQuery from "../hooks/boards/use-board-query";
 
@@ -11,7 +12,10 @@ type Props = {
   index: number;
   title: string;
   slug: string;
+  totalChecks?: number;
+  totalCompletedChecks?: number;
   hasDescription?: boolean;
+  hasChecklist?: boolean;
 };
 
 const Card: React.FC<Props> = ({
@@ -20,9 +24,14 @@ const Card: React.FC<Props> = ({
   index,
   title,
   slug,
+  totalChecks,
+  totalCompletedChecks,
   hasDescription,
+  hasChecklist,
 }) => {
   const { data: board } = useBoardQuery(boardId);
+
+  const isAllChecked = totalChecks === totalCompletedChecks;
 
   return (
     <Draggable draggableId={`card-${id}`} index={index}>
@@ -47,9 +56,26 @@ const Card: React.FC<Props> = ({
                 {hasDescription && (
                   <div className="flex items-center mt-2">
                     {hasDescription && (
-                      <span className="text-slate-500">
+                      <span className="text-slate-500 mr-2">
                         <MdSubject size={20} />
                       </span>
+                    )}
+                    {hasChecklist && (
+                      <div
+                        className={classnames(
+                          "flex items-center rounded p-1 pr-2",
+                          {
+                            "bg-green-500": isAllChecked,
+                            "text-white": isAllChecked,
+                            "text-slate-500": !isAllChecked,
+                          }
+                        )}
+                      >
+                        <MdOutlineCheckBox className="mr-1" size={20} />
+                        <span className="text-xs">
+                          {totalCompletedChecks}/{totalChecks}
+                        </span>
+                      </div>
                     )}
                   </div>
                 )}
