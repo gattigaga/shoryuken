bb3d6916b932596ad137d6bfa3d40c4b24f86fa6
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-06-07 20:05:50 +0700

    Refactor BoardTitle component

--- components/BoardTitle.tsx
+++ components/BoardTitle.tsx
@@ -1,24 +1,45 @@
 import React, { useEffect, useRef, useState } from "react";
+import toast from "react-hot-toast";
+
+import useBoardQuery from "../hooks/boards/use-board-query";
+import useUpdateBoardMutation from "../hooks/boards/use-update-board-mutation";
 
 type Props = {
-  value: string;
-  onSubmit?: (value: string) => void;
+  id: number;
 };
 
-const EditableBoardTitle: React.FC<Props> = ({ value, onSubmit }) => {
+const BoardTitle: React.FC<Props> = ({ id }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [inputWidth, setInputWidth] = useState(0);
   const refText = useRef<HTMLHeadingElement>(null);
   const refInput = useRef<HTMLInputElement>(null);
+  const { data: board } = useBoardQuery(id);
+  const updateBoardMutation = useUpdateBoardMutation();
+
+  const updateTitle = async (title: string) => {
+    if (!title) return;
+
+    try {
+      await updateBoardMutation.mutateAsync({
+        id: board.id,
+        body: {
+          title,
+        },
+      });
+    } catch (error) {
+      toast.error("Failed to update board title.");
+    }
+  };
 
   useEffect(() => {
     if (refText.current) {
       setInputWidth(refText.current.offsetWidth + 24);
     }
-  }, [value]);
+  }, [board.title]);
 
   return (
     <div>
+      {/* Static mode */}
       {!isEditing && (
         <button
           type="button"
@@ -34,17 +55,19 @@ const EditableBoardTitle: React.FC<Props> = ({ value, onSubmit }) => {
             ref={refText}
             className="text-white text-lg font-semibold inline-block"
           >
-            {value}
+            {board.title}
           </h1>
         </button>
       )}
+
+      {/* Edit mode */}
       {isEditing && (
         <input
           ref={refInput}
           className="py-0 px-1 rounded text-lg font-semibold"
           style={{ width: inputWidth }}
           type="text"
-          defaultValue={value}
+          defaultValue={board.title}
           onKeyDown={(event) => {
             if (["Enter", "Escape"].includes(event.key)) {
               refInput.current?.blur();
@@ -52,7 +75,7 @@ const EditableBoardTitle: React.FC<Props> = ({ value, onSubmit }) => {
           }}
           onBlur={(event) => {
             setIsEditing(false);
-            onSubmit?.(event.target.value);
+            updateTitle(event.target.value);
           }}
         />
       )}
@@ -60,4 +83,4 @@ const EditableBoardTitle: React.FC<Props> = ({ value, onSubmit }) => {
   );
 };
 
-export default EditableBoardTitle;
+export default BoardTitle;
