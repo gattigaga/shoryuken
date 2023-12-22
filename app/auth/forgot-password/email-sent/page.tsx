2e211b515f5c8c518f8d5233646db2ae68e87058
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2023-12-23 01:35:57 +0700

    Migrate forgot password email sent page to app router

--- app/auth/forgot-password/email-sent/page.tsx
+++ app/auth/forgot-password/email-sent/page.tsx
@@ -1,39 +1,17 @@
-import React, { useEffect, useState } from "react";
-import Head from "next/head";
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 import Button from "../../../components/Button";
+import Email from "./components/Email";
+
+export const metadata: Metadata = {
+  title: "Email Sent | Shoryuken",
+};
 
 const EmailSentPage = () => {
-  const [email, setEmail] = useState("");
-  const router = useRouter();
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const newEmail = sessionStorage.getItem("forgotPasswordEmail") || "";
-
-      if (!newEmail) {
-        router.replace("/auth/forgot-password");
-        return;
-      }
-
-      setEmail(newEmail);
-      return;
-    }
-  }, []);
-
-  if (!email) {
-    return null;
-  }
-
   return (
     <div className="min-h-screen md:bg-slate-50">
-      <Head>
-        <title>Email sent | Shoryuken</title>
-      </Head>
-
       <main className="py-12">
         <div className="w-80 mx-auto md:w-96">
           <div className="flex justify-center mb-6">
@@ -54,9 +32,9 @@ const EmailSentPage = () => {
               To reset your password, click the recovery link in the email
               we&lsquo;ve sent to
             </p>
-            <p className="text-sm font-semibold text-slate-600 leading-normal mb-8">
-              {email}
-            </p>
+
+            <Email />
+
             <Link href="/auth/signin">
               <Button className="w-full">Back to Sign In</Button>
             </Link>
