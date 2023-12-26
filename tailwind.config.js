b425ae7b17c2a15038f9b9c4d755db315fc2400c
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2023-12-27 00:54:21 +0700

    Update tailwindcss version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       sans: ["Poppins"],
