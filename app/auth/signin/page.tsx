91179fba715d13bdb5bcfcceec8072576223a456
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2023-12-31 15:34:13 +0700

    Implement localization for sign in page

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -24,28 +24,8 @@ const SignInPage = () => {
               />
             </Link>
           </div>
-          <div className="md:rounded md:bg-white md:shadow-lg md:p-8">
-            <h1 className="font-semibold text-center text-slate-600 mb-6">
-              Sign in to Shoryuken
-            </h1>
 
-            <Form />
-
-            <div className="w-full border-t my-6" />
-            <div className="flex justify-center items-center">
-              <Link href="/auth/forgot-password">
-                <p className="text-xs text-blue-700 text-center">
-                  Can&lsquo;t sign in?
-                </p>
-              </Link>
-              <span className="mx-3 text-slate-600">&#8226;</span>
-              <Link href="/auth/signup">
-                <p className="text-xs text-blue-700 text-center">
-                  Sign up for an account
-                </p>
-              </Link>
-            </div>
-          </div>
+          <Form />
         </div>
       </main>
     </div>
