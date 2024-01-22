233710541208423d4c2301c79d2ae9f78582feba
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2024-01-22 22:33:08 +0700

    Fix redirection loading in home page

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import { FC } from "react";
-import { useSearchParams } from "next/navigation";
+import { FC, useEffect, useState } from "react";
 import Spinner from "react-spinners/ScaleLoader";
 
 const Loading: FC = () => {
-  const searchParams = useSearchParams();
+  const [isLoading, setIsLoading] = useState(false);
 
-  if (!searchParams?.get("access_token")) {
-    return null;
-  }
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const queryString = window.location.hash.replace("#", "");
+      const params = new URLSearchParams(queryString);
+
+      setIsLoading(!!params.get("access_token"));
+    }
+  }, []);
+
+  if (!isLoading) return null;
 
   return (
     <div className="w-full h-screen flex justify-center items-center fixed top-0 left-0 z-10 bg-white">
