c8c0830be09c6da59c5ccde1230b71b988c6e1ea
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2025-11-29 22:30:43 +0700

    Implement remaining tables schema

--- app/config/db/schema.ts
+++ app/config/db/schema.ts
@@ -8,3 +8,55 @@ export const usersTable = sqliteTable("users", {
   email_confirmed_at: text(),
   avatar: text(),
 });
+
+export const boardsTable = sqliteTable("boards", {
+  id: integer().primaryKey({ autoIncrement: true }),
+  user_id: integer().notNull(),
+  title: text().notNull(),
+  slug: text().notNull(),
+  color: text().notNull(),
+  created_at: text(),
+});
+
+export const listsTable = sqliteTable("lists", {
+  id: integer().primaryKey({ autoIncrement: true }),
+  board_id: integer().notNull(),
+  index: integer().notNull(),
+  title: text().notNull(),
+  created_at: text(),
+});
+
+export const cardsTable = sqliteTable("cards", {
+  id: integer().primaryKey({ autoIncrement: true }),
+  list_id: integer().notNull(),
+  index: integer().notNull(),
+  title: text().notNull(),
+  description: text().default(""),
+  slug: text().notNull(),
+  has_checklist: integer().notNull().default(0),
+  created_at: text(),
+});
+
+export const checksTable = sqliteTable("checks", {
+  id: integer().primaryKey({ autoIncrement: true }),
+  card_id: integer().notNull(),
+  index: integer().notNull(),
+  content: text().notNull(),
+  is_checked: integer().notNull().default(0),
+  created_at: text(),
+});
+
+export const dueDatesTable = sqliteTable("due_dates", {
+  id: integer().primaryKey({ autoIncrement: true }),
+  card_id: integer().notNull(),
+  timestamp: text().notNull(),
+  is_done: integer().notNull().default(0),
+  created_at: text(),
+});
+
+export const boardMembersTable = sqliteTable("board_members", {
+  id: integer().primaryKey({ autoIncrement: true }),
+  board_id: integer().notNull(),
+  user_id: integer().notNull(),
+  created_at: text(),
+});
